4ba4b6bfd1e2f9ff7220dc272a185755323e6f35
Pengqi Yin <yinpengqi@bupt.edu.cn>
2019-09-23 02:35:22 -0700

    add multipthread test code

---
+++ multithread.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<pthread.h>
+
+void *printNumber(void *argv)
+{
+	int upper = atoi(argv);
+	for(int i=0;i<upper;i++)
+	{
+		printf("%d\n", i);
+		sleep(1);
+	}
+	pthread_exit(0);
+}
+
+int main(int argc, char *argv[])
+{
+	pthread_t td;
+	pthread_attr_t attr;
+	pthread_attr_init(&attr);
+	pthread_create(&td, &attr, printNumber, argv[1]);
+	
+	pthread_join(td, NULL);
+
+	fprintf(stderr, "%s\n", "Success");
+
+}
